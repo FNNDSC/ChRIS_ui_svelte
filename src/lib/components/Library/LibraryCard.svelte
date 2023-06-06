ddb81ffb3765cabd0ade33efd7b85e3f08ede848
PintoGideon <gideonpinto123@gmail.com>
2023-06-05 21:37:58 -0400

    Creating Components with slots for reusability

---
+++ src/lib/components/Library/LibraryCard.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Card } from "$components/ui/card";
+  export let path: string;
+
+ 
+</script>
+
+<Card
+  class="relative flex items-center px-6 py-5 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-200"
+>
+  <slot name="icon" />
+  <div class="min-w-0 flex-1">
+    <a href={path} class="focus:outline-none">
+      <span class="absolute inset-0" aria-hidden="true" />
+      <span class="flex items-center">
+        <slot name="name" />
+      </span>
+    </a>
+  </div>
+</Card>
--- src/routes/(_main)/uploads/[...slug]/+page.svelte
+++ src/routes/(_main)/uploads/[...slug]/+page.svelte
@@ -1,28 +1,36 @@
 <script lang="ts">
   import { page } from "$app/stores";
+  import LibraryCard from "$components/Library/LibraryCard.svelte";
+  import type { FeedFile } from "@fnndsc/chrisapi";
   import type { PageData } from "./$types";
-  import { Card } from "$components/ui/card";
-  import { Folder } from "lucide-svelte";
+  import { Folder, FileArchive } from "lucide-svelte";
 
   export let data: PageData;
   $: pathname = $page.url.pathname;
+
+  function getFileName(file: FeedFile["data"]) {
+    const fileNameArray = file.fname.split("/");
+    const fileName = fileNameArray[fileNameArray.length - 1];
+    return fileName;
+  }
 </script>
 
-<div class="grid gap-4 sm:grid-cols-1 lg:grid-cols-6">
+<div class="grid gap-4 sm:grid-cols-1 lg:grid-cols-5">
   {#each data.folders as folder (folder.name)}
-    <Card
-      class="
-       relative flex items-center px-6 py-5 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-200"
-    >
-      <div class="min-w-0 flex-1">
-        <a href={`${pathname}/${folder.name}`} class="focus:outline-none">
-          <span class="absolute inset-0" aria-hidden="true" />
-          <span class="flex items-center">
-            <Folder class="mr-2" />
-            <p class="text-sm truncate font-medium text-white">{folder.name}</p>
-          </span>
-        </a>
-      </div>
-    </Card>
+    <LibraryCard path={`${pathname}/${folder.name}`}>
+      <Folder slot="icon" class="mr-2" />
+      <p slot="name" class="text-sm truncate font-medium text-white">
+        {folder.name}
+      </p>
+    </LibraryCard>
+  {/each}
+
+  {#each data.files as file (file.fname)}
+    <LibraryCard path={""}>
+      <FileArchive slot="icon" class="mr-2" />
+      <p slot="name" class="text-sm truncate font-medium text-white">
+        {getFileName(file) || ""}
+      </p>
+    </LibraryCard>
   {/each}
 </div>
